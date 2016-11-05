b4273598ac3d77b835833801da8afd4d89afcc80
Atikhun Orisoon <genatorster@gmail.com>
2016-11-06 00:31:00 +0700

    Add more fillable fields in model User for mass assignment support

--- nudmore-main-site/app/User.php
+++ nudmore-main-site/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'password', 'email', 'title', 'name', 'surname', 'gender', 'identity_number'
     ];
 
     /**
