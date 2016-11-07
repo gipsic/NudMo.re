474840c7a4b9fd76f30a48e43875c1c5e8638147
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2016-11-07 20:23:23 +0700

    Create email notifications table

---
+++ nudmore-main-site/database/migrations/2016_11_07_195929_create_email_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('notification_id')->unique();
+            $table->string('email_address');
+            $table->string('topic');
+            $table->string('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_notifications');
+    }
+}
