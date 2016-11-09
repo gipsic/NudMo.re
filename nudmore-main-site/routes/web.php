3748c9c2e30ba54f1de0c90c0354c610a42f0ebc
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2016-11-09 09:54:14 +0700

    Refactors

--- nudmore-main-site/routes/web.php
+++ nudmore-main-site/routes/web.php
@@ -20,17 +20,11 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::get('/profile', 'ProfileController@index')->middleware(['auth']);
-
 Route::get('/profiles', 'ProfileController@list')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
 Route::get('/profile/{id}', 'ProfileController@showUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
-
 Route::get('/profile/{id}/edit', 'ProfileController@showEditUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
-
 Route::post('/profile/{id}/edit', 'ProfileController@editUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
-
 Route::delete('/profile/{id}/delete', 'ProfileController@deleteUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
-
 Route::get('/create_user', 'ProfileController@showCreateUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
-
 Route::post('create_user', 'ProfileController@createUser')->middleware(['auth', /*'doctor', 'staff', 'nurse', 'pharmacist', 'administrator'*/]);
 
