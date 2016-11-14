846aaee0f8178b739336f7754511d055144ce0e2
Wisit Wongchaianukul <gipsicweb@gmail.com>
2016-11-14 09:04:27 +0700

    แก้ Mitigration

--- nudmore-main-site/database/migrations/2016_11_09_231518_modify_necessary_table2.php
+++ nudmore-main-site/database/migrations/2016_11_09_231518_modify_necessary_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyNecessaryTable extends Migration
+class ModifyNecessaryTable2 extends Migration
 {
     /**
      * Run the migrations.
