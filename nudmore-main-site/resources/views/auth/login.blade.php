5ae19574e8c288c54559909c436d1d8308ba943f
Wisit Wongchaianukul <gipsicweb@gmail.com>
2016-11-19 15:20:27 +0700

    Login Page

--- nudmore-main-site/resources/views/auth/login.blade.php
+++ nudmore-main-site/resources/views/auth/login.blade.php
@@ -13,101 +13,34 @@
 		<div class="panel-body">
 			<p class="text-center pv">ลงชื่อเข้าใช้เพื่อดำเนินการต่อ</p>
 			{!! Form::open(['url' => 'login', 'class' => ' mb-lg']) !!}
-                        {!! Form::token() !!}
-				<div class="form-group has-feedback">
-					<input id="inputUsername" type="username" placeholder="ชื่อผู้ใช้" autocomplete="off" required 
-						   class="form-control">
+                {!! Form::token() !!}
+				<div class="form-group has-feedback {{ $errors->has('username') ? ' has-error' : '' }}">
+					{!! Form::text('username', old('username'), ['class' => 'form-control', 'placeholder' => 'ชื่อผู้ใช้', 'required', 'autofocus']) !!}
 					<span class="fa fa-user form-control-feedback text-muted"></span>
 				</div>
-				<div class="form-group has-feedback">
-					<input id="inputPassword" type="password" placeholder="รหัสผ่าน" required class="form-control">
+				@if ($errors->has('username'))<span class="help-block"><strong>{{ $errors->first('username') }}</strong></span>@endif
+				<div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
+					{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'รหัสผ่าน', 'required']) !!}
 					<span class="fa fa-lock form-control-feedback text-muted"></span>
 				</div>
+				@if ($errors->has('password'))<span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>@endif
 				<div class="clearfix">
 					<div class="checkbox c-checkbox pull-left mt0">
-						<label>
-							<input type="checkbox" value="" name="remember">
-							<span class="fa fa-check"></span>จดจำรหัสผ่าน</label>
-					</div> <a class="btn btn-link" href>
-					<div class="pull-right"><a href="recover.html" class="text-muted">ลืมรหัสผ่าน?</a>
+						<label>{!! Form::checkbox('remember') !!} <span class="fa fa-check"></span>จดจำรหัสผ่าน</label>
+					</div>
+					<div class="pull-right">
+						<a href="{{ url('/password/reset') }}" class="text-muted">ลืมรหัสผ่าน?</a>
 					</div>
 				</div>
-				<button type="submit" class="btn btn-block btn-primary mt-lg">เข้าสู่ระบบ</button>
+				{!! Form::submit('เข้าสู่ระบบ', ['class' => 'btn btn-block btn-primary mt-lg']) !!}
 				<a href="{{ url('/register') }}" class="btn btn-block btn-default">สมัครสมาชิก</a>
 			{!! Form::close() !!}
 		</div>
 	</div>
 	<!-- END panel-->
 	<div class="p-lg text-center">
-		<span>&copy;</span>
-		<span>2016</span>
-		<span>-</span>
-		<span>NudMo.re</span>
-		<br>
-		<span>ระบบนัดหมายแพทย์ NudMore</span>
+		<span>&copy; 2016 - NudMo.re</span><br><span>ระบบนัดหมายแพทย์ NudMore</span>
 	</div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    {!! Form::open(['url' => 'login', 'class' => 'form-horizontal']) !!}
-                        {!! Form::token() !!}
-
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            {!! Form::label('username', 'Username', ['class' => 'col-md-4 control-label']) !!}
-
-                            <div class="col-md-6">
-                                {!! Form::text('username', old('username'), ['class' => 'form-control', 'required', 'autofocus']) !!}
-
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            {!! Form::label('password', 'Password', ['class' => 'col-md-4 control-label']) !!}
-
-                            <div class="col-md-6">
-                                {!! Form::password('password', ['class' => 'form-control', 'required']) !!}
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        {!! Form::checkbox('remember') !!} Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                {!! Form::submit('Login', ['class' => 'btn btn-primary']) !!}
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
--- nudmore-main-site/routes/web.php
+++ nudmore-main-site/routes/web.php
@@ -18,8 +18,8 @@ Route::get('/home', 'HomeController@index');
 
 // Profile Route
 Route::get('/profile/list', 'ProfileController@list')->middleware(['auth', 'activated', 'staffteam']);
-Route::get('/profile/create', 'ProfileController@showCreateUser')/*->middleware(['auth', 'activated', 'administrator'])*/;
-Route::post('/profile/create', 'ProfileController@createUser')/*->middleware(['auth', 'activated', 'administrator'])*/;
+Route::get('/profile/create', 'ProfileController@showCreateUser')->middleware(['auth', 'activated', 'administrator']);
+Route::post('/profile/create', 'ProfileController@createUser')->middleware(['auth', 'activated', 'administrator']);
 Route::get('/profile/edit', 'ProfileController@showEditUser')->middleware(['auth', 'activated']);
 Route::post('/profile/edit', 'ProfileController@editUser')->middleware(['auth', 'activated']);
 Route::get('/profile/{id}', 'ProfileController@showUser')->middleware(['auth', 'activated', 'staffteam']);
