ee83cddcafdb43890f3f8356b1c0b84cc67189cf
Wisit Wongchaianukul <gipsicweb@gmail.com>
2016-11-21 19:51:39 +0700

    แก้ SMS - curl SSL

--- nudmore-main-site/app/Http/Controllers/SmsNotificationController.php
+++ nudmore-main-site/app/Http/Controllers/SmsNotificationController.php
@@ -22,6 +22,7 @@ class SmsNotificationController extends Controller
 
 		curl_setopt($ch, CURLOPT_URL,"https://sms.gipsic.com/api/send");
 		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_POSTFIELDS,
 		            'key=1Wa572Da9Lf82gImMOEh9N8XYYd0ZyfT&secret=8f4VzC9j4zZ914fajzT925y9e8uE5EOq&phone='.$phone_number."&sender=NOTICE&message=".$message);
 
@@ -30,11 +31,13 @@ class SmsNotificationController extends Controller
 		$server_output = curl_exec ($ch);
 
 		curl_close ($ch);
+		
+		$out = json_decode($server_output);
 
-		if ($server_output == "OK") {
-
+		if ($out['message'] == "OK") {
+			echo 'ok';
 		} else {
-
+			print_r($out);
 		}
     }
 }
